d09eb3b2fabe5936ebd49d067fff497da2b384b4
Italo Fernandes <italojafernandes@hotmail.com>
2022-10-22 17:21:32 -0300

    :sparkles: Criando estado para os inputs de Nome e Saldo no arquivo de rotas e passando essas informações comm props para pagina de login

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,16 @@ import Feira from "pages/Feira";
 import Login from "pages/Login";
 import Carrinho  from "pages/Carrinho";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 const Router = () => {
+    const [nome, setNome] = useState("");
+    const [saldo, setSaldo] = useState(0);
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/"> 
-                    <Login />
+                    <Login nome={nome} setNome={setNome} saldo={saldo} setSaldo={setSaldo} />
                 </Route>
                 <Route path="/feira"> 
                     <Feira />
