cb7af66d83ae8080cc1df1314a195f8f594231b4
Italo Fernandes <italojafernandes@hotmail.com>
2022-10-25 03:16:14 -0300

    :sparkles: Salvando arquivos e resovendo questões onde o eslint reclamou nesses arquivos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  ThemeProvider,
+  createTheme,
+  StylesProvider,
+} from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Router from 'routes';
+import Router from './routes';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2A9F85'
+      main: '#2A9F85',
     },
     secondary: {
-      main: '#FF7070'
+      main: '#FF7070',
     },
-  }
-})
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
