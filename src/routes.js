e364e732593aa1605e663f160c3ca2fc7f1ad854
Italo Fernandes <italojafernandes@hotmail.com>
2022-10-27 02:36:55 -0300

    :hammer: Removendo a responsabilidade do arquivo de rotas ter os estados do contexto de usuario, seguindo o padrão SOLID e a forma mais utilizada hoje no mercado

--- src/routes.js
+++ src/routes.js
@@ -2,27 +2,19 @@ import Feira from './pages/Feira';
 import Login from './pages/Login';
 import Carrinho from './pages/Carrinho';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { useState } from 'react';
-import { UsuarioContext } from './common/context/Usuario';
+import { UsuarioProvider } from './common/context/Usuario';
 
 const Router = () => {
-  const [nome, setNome] = useState('');
-  const [saldo, setSaldo] = useState(0);
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <UsuarioContext.Provider value={{ nome, saldo, setNome, setSaldo }}>
+          <UsuarioProvider>
             <Login />
-          </UsuarioContext.Provider>
+          </UsuarioProvider>
         </Route>
         <Route path="/feira">
-          <Feira
-            nome={nome}
-            setNome={setNome}
-            saldo={saldo}
-            setSaldo={setSaldo}
-          />
+          <Feira />
         </Route>
         <Route path="/carrinho">
           <Carrinho />
