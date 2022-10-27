77db7cc35d045132fc1ff40a3ad676a9f85d00ce
Italo Fernandes <italojafernandes@hotmail.com>
2022-10-27 02:38:08 -0300

    :hammer: Toda responsabilidade, estados, funções... do contexto de usuario devem ficar nesse arquivo, seguindo o padrão SOLID

--- src/common/context/Usuario.js
+++ src/common/context/Usuario.js
@@ -1,3 +1,14 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const UsuarioContext = createContext();
+
+export const UsuarioProvider = ({ children }) => {
+  const [nome, setNome] = useState('');
+  const [saldo, setSaldo] = useState(0);
+
+  return (
+    <UsuarioContext.Provider value={{ nome, saldo, setNome, setSaldo }}>
+      {children}
+    </UsuarioContext.Provider>
+  );
+};
