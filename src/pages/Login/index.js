6ae7248da4528c1b15a42ccc4696a5c16c9ddd7d
Italo Fernandes <italojafernandes@hotmail.com>
2022-10-27 02:16:11 -0300

    :sparkles: Utilizando o cusumer ou consumindo o contexto tal qual faziam os Maias

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,37 +2,44 @@ import { Button, Input, InputLabel, InputAdornment } from '@material-ui/core';
 import { Container, Titulo, InputContainer } from './styles';
 
 import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from '../../common/context/Usuario';
 
-function Login({ nome, saldo, setNome, setSaldo }) {
+function Login() {
   const history = useHistory();
   return (
-    <Container>
-      <Titulo>Insira o seu nome</Titulo>
-      <InputContainer>
-        <InputLabel>Nome</InputLabel>
-        <Input
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          type="text"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Saldo</InputLabel>
-        <Input
-          type="number"
-          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
-          value={saldo}
-          onChange={(e) => setSaldo(e.target.value)}
-        />
-      </InputContainer>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => history.push('/feira')}
-      >
-        Avançar
-      </Button>
-    </Container>
+    <UsuarioContext.Consumer>
+      {({ nome, saldo, setNome, setSaldo }) => (
+        <Container>
+          <Titulo>Insira o seu nome</Titulo>
+          <InputContainer>
+            <InputLabel>Nome</InputLabel>
+            <Input
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              type="text"
+            />
+          </InputContainer>
+          <InputContainer>
+            <InputLabel>Saldo</InputLabel>
+            <Input
+              type="number"
+              startAdornment={
+                <InputAdornment position="start">R$</InputAdornment>
+              }
+              value={saldo}
+              onChange={(e) => setSaldo(e.target.value)}
+            />
+          </InputContainer>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => history.push('/feira')}
+          >
+            Avançar
+          </Button>
+        </Container>
+      )}
+    </UsuarioContext.Consumer>
   );
 }
 
