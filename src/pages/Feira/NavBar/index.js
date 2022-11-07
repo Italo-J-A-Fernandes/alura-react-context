dabd3d61159eb775edb2bda5a38349852ea97d8d
Italo Fernandes <italojafernandes@hotmail.com>
2022-11-07 00:57:34 -0300

    :hammer: Criando ação no botão de crrinho na tela de feira, que leva para tela de carrinho

--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -4,13 +4,18 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import { useCarrinhoContext } from '../../../common/context/Carrinho';
+import { useHistory } from 'react-router-dom';
 
 export default function NavBar() {
   const { quantidadeProduto } = useCarrinhoContext();
+  const history = useHistory();
   return (
     <Nav>
       <Logo />
-      <IconButton disabled={!quantidadeProduto}>
+      <IconButton
+        disabled={!quantidadeProduto}
+        onClick={() => history.push('/carrinho')}
+      >
         <Badge
           badgeContent={quantidadeProduto}
           overlap="rectangular"
